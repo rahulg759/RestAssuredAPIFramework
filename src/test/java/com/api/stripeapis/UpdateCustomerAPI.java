321660a355cb5b7976e28d9274e7509487246f1d
Rahul Gupta <rahulg759@gmail.com>
2020-07-14 19:50:34 +0530

    Added explanation files

--- src/test/java/com/api/stripeapis/UpdateCustomerAPI.java
+++ src/test/java/com/api/stripeapis/UpdateCustomerAPI.java
@@ -1,5 +1,22 @@
 package com.api.stripeapis;
 
-public class UpdateCustomerAPI {
+import static io.restassured.RestAssured.given;
 
+import java.util.Hashtable;
+
+import com.api.base.BaseTest;
+
+import io.restassured.response.Response;
+
+public class UpdateCustomerAPI extends BaseTest{
+
+	public static Response sendPostRequesttoUpdateCustomerAPIWithValidAuthKey(Hashtable<String,String> data) {
+		Response response=given().auth().basic(prop.getProperty("validSecretKey"), "")
+				.formParam("email",data.get("email"))
+				.formParam("name",data.get("name"))
+				.formParam("description",data.get("description"))
+				.post(prop.getProperty("customerAPIEndPoint"));
+
+		return response;
+	}
 }
